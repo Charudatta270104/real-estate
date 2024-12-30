ed00eb01e185157900a0f54828b92bc4799ab000
Charudatta270104 <charudattawarke098@gmail.com>
2024-12-30 23:54:01 +0530

    css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home1 from "./page/Home1";
-
+import "../src/assets/css/style.css"
 function App() {
   return (
     <Router>
