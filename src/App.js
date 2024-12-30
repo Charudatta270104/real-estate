e142b36bc386906fd51a72c5114b457872e94cbf
Charudatta270104 <charudattawarke098@gmail.com>
2024-12-30 23:59:24 +0530

    modify app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home1 from "./page/Home1";
-import "../src/assets/css/style.css"
+import "../src/assets/css/style.css";
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="*" element={<Home1 />} />
+        <Route path="/" element={<Home1 />} />
       </Routes>
     </Router>
   );
